dd4075b8cfda7c511e0701c2b494615a7fb4d1af
Nolhan <nolhan.marteau@gmail.com>
2026-01-16 15:44:04 +0100

    Feat : Ajout route LoginUser

---
+++ Backend - PortFolio/src/routes/users/LoginUser.js
@@ -0,0 +1,69 @@
+import express from "express";
+import mysql from "mysql2/promise";
+import { z } from "zod";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { configDotenv } from 'dotenv';
+
+configDotenv();
+
+const SecretKey = process.env.jwtKey;
+const router = express.Router();
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+});
+
+const PostSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+});
+
+router.post("/", async (req, res) => {
+    try {
+
+        const { email, password } = PostSchema.parse(req.body);
+
+        const [rows] = await pool.query( 
+            "SELECT id, name, firstname, email, password FROM users WHERE email = ?", 
+            [email]
+        );
+
+        if (rows.length === 0) { 
+            res.status(404);
+            res.json({ message : "Aucun compte n'a été enregistré avec cette adresse Mail" });
+            return;
+
+        } else { 
+
+            const user = rows[0]; 
+
+            const decryptpassword = await bcrypt.compare(password, user.password);
+            if (decryptpassword){
+                res.status(200)
+
+                const payload = { userId: user.id, email: user.email, name : user.name, firstname : user.firstname }; 
+
+                const token = jwt.sign(payload, SecretKey, { expiresIn: "1h" });
+                res.json({ token });
+                return;
+
+            } else {
+                res.status(401);
+                res.json({ message: "Mot de passe incorrect" });
+                return;
+            }
+        }
+
+    } catch (error) {
+
+        res.status(500);
+        res.json({ error: error.message });
+
+    }
+});
+
+export default router;
--- Backend - PortFolio/src/server.js
+++ Backend - PortFolio/src/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 
 // Routes Users
 import CreateUser from './routes/users/CreateUser.js';
+import LoginUser from './routes/users/LoginUser.js';
 
 // Route Projects
 
@@ -13,6 +14,7 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use('/SignUP', CreateUser);
+app.use('/SignIN', LoginUser);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
